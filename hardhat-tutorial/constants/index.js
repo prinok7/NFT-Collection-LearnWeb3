46a319a26863b2baa833c36115135b8882ae0970
Prinok7 <Prinok2015@gmail.com>
2022-02-05 22:40:48 +0100

    Adding NFT Collections from Learnweb3 Tutorial on Learnweb3.io

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x051fc993a6e1583BD1d6da92aC7705d9aB6dDfa6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
